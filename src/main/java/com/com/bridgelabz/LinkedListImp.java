aeaf2ac479201ebb4d8be183331d36097379c1df
pranit67 <pranit67@gmail.com>
2023-01-25 21:12:09 +0530

    Added: code for Simple Linked List

--- src/main/java/com/com/bridgelabz/LinkedListImp.java
+++ src/main/java/com/com/bridgelabz/LinkedListImp.java
@@ -1,7 +1,34 @@
 package com.com.bridgelabz;
 
 public class LinkedListImp {
+    private Node head;
+
+    private static class Node {
+        Integer data;
+        Node next;
+
+        public Node(Integer data) {
+            this.data = data;
+            this.next = null;
+        }
+    }
+
+    public void printList() {
+        while (head != null) {
+            System.out.print(head.data + " ");
+            head = head.next;
+        }
+    }
+
     public static void main(String[] args) {
-        System.out.println("Welcome to Linked List Implementation.");
+        LinkedListImp linkedListImp = new LinkedListImp();
+        linkedListImp.head = new Node(56);
+        Node second = new Node(30);
+        Node third = new Node(70);
+
+        linkedListImp.head.next = second;
+        second.next = third;
+
+        linkedListImp.printList();
     }
 }
