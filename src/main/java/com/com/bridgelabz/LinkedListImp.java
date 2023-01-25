bc1661a39ed341c5a151618c19490017257835ac
pranit67 <pranit67@gmail.com>
2023-01-25 21:46:39 +0530

    Added: code for Apending the Node 30, 70, 56

--- src/main/java/com/com/bridgelabz/LinkedListImp.java
+++ src/main/java/com/com/bridgelabz/LinkedListImp.java
@@ -13,11 +13,29 @@ public class LinkedListImp {
         }
     }
 
-    public void addingNode(Integer data) {
+    /* Adding Node by using add method  */
+//    public void addingNode(Integer data) {
+//
+//        Node newNode = new Node(data);
+//        newNode.next = head;
+//        head = newNode;
+//    }
+    /*Adding data by Append method */
+    public void appendNode(Integer data) {
 
         Node newNode = new Node(data);
-        newNode.next = head;
-        head = newNode;
+
+        if (head == null) {
+            head = new Node(data);
+            return;
+        }
+        newNode.next = null;
+
+        Node last = head;
+        while (last.next != null)
+            last = last.next;
+        last.next = newNode;
+        return;
     }
 
     public void printList() {
@@ -29,10 +47,12 @@ public class LinkedListImp {
 
     public static void main(String[] args) {
         LinkedListImp linkedListImp = new LinkedListImp();
-        linkedListImp.addingNode(70);
-        linkedListImp.addingNode(30);
-        linkedListImp.addingNode(56);
-
+//        linkedListImp.addingNode(70);
+//        linkedListImp.addingNode(30);
+//        linkedListImp.addingNode(56);
+        linkedListImp.appendNode(56);
+        linkedListImp.appendNode(30);
+        linkedListImp.appendNode(70);
         linkedListImp.printList();
     }
 }
