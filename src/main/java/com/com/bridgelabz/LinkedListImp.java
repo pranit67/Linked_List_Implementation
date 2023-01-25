40c067c81b041a02e3139eaf2506c8996ad5a60a
pranit67 <pranit67@gmail.com>
2023-01-25 16:13:05 +0530

    Added: Code For Welcome Message

---
+++ src/main/java/com/com/bridgelabz/LinkedListImp.java
@@ -0,0 +1,7 @@
+package com.com.bridgelabz;
+
+public class LinkedListImp {
+    public static void main(String[] args) {
+        System.out.println("Welcome to Linked List Implementation.");
+    }
+}
